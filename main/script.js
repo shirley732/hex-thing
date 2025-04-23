d4cdb6ee776c58132859ea31c2ad5897f355f34c
kade9412 <debord253@student.pcsstn.net>
2025-04-23 11:28:49 -0500

    calc function

--- main/script.js
+++ main/script.js
@@ -5,14 +5,13 @@ const binaryEl = document.querySelector("#binaryOutput");
 
 inputEl.value = Math.round(Math.random() * (Math.random() * (Math.random() * 1000) * Math.random()));
 
-inputEl.addEventListener("input", e => {
-    const num = inputEl.value;
+inputEl.addEventListener("input", calculate(Number(inputEl.value)));
 
-    hexEl.textContent = `0x${parseInt(num).toString(16)}`;
-    octalEl.textContent = `0o${parseInt(num).toString(8)}`;
-    binaryEl.textContent = `0b${(num >>> 0).toString(2)}`;
-});
-
+function calculate(num){
+    hexEl.textContent = `0x${Number(num).toString(16)}`;
+    octalEl.textContent = `0o${Number(num).toString(8)}`;
+    binaryEl.textContent = `0b${Number(num).toString(2)}`;
+};
 document.addEventListener("wheel", (event) => {
     if (event.deltaY < 0) {
         inputEl.value = Number(inputEl.value) + 1;
