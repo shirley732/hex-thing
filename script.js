0ba7a7ddf3e38e4d8271e355b1f4ca8b7ad8b33d
shirley732 <123391202+shirley732@users.noreply.github.com>
2025-04-23 10:56:19 -0500

    removed the length checker

--- script.js
+++ script.js
@@ -9,11 +9,6 @@ inputEl.addEventListener("input", e => {
     hexEl.textContent = `0x ${parseInt(num).toString(16)}`;
     octalEl.textContent = `0o${parseInt(num).toString(8)}`;
     binaryEl.textContent = `0b${(num >>> 0).toString(2)}`;
-
-    
-    if (inputEl.value.length > 20) {
-        window.close();
-    }
 });
 
 document.addEventListener("scroll", e => {
