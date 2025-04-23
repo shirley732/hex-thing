284169e99c7ae3998616289c90aaf8df59772eaa
Cadmium <133188056+CadmanW@users.noreply.github.com>
2025-04-23 11:20:43 -0500

    added random number on reload

--- main/script.js
+++ main/script.js
@@ -3,14 +3,12 @@ const hexEl = document.querySelector("#hexOutput");
 const octalEl = document.querySelector("#octalOutput");
 const binaryEl = document.querySelector("#binaryOutput");
 
+inputEl.value = Math.round(Math.random() * (Math.random() * (Math.random() * 1000) * Math.random()));
+
 inputEl.addEventListener("input", e => {
     const num = inputEl.value;
 
     hexEl.textContent = `0x${parseInt(num).toString(16)}`;
     octalEl.textContent = `0o${parseInt(num).toString(8)}`;
     binaryEl.textContent = `0b${(num >>> 0).toString(2)}`;
-});
-
-document.addEventListener("scroll", e => {
-    console.log(e);
 });
