28ecce311ffb5602ba40e2735c728f9728c471c0
hill145 <hill145@student.pcsstn.net>
2025-04-23 11:00:14 -0500

    space. no more space.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const binaryEl = document.querySelector("#binaryOutput");
 inputEl.addEventListener("input", e => {
     const num = inputEl.value;
 
-    hexEl.textContent = `0x ${parseInt(num).toString(16)}`;
+    hexEl.textContent = `0x${parseInt(num).toString(16)}`;
     octalEl.textContent = `0o${parseInt(num).toString(8)}`;
     binaryEl.textContent = `0b${(num >>> 0).toString(2)}`;
 
